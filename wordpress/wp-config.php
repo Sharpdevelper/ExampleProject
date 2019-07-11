9fad161ec18568e254ce4ea1de8b4faa72f52ce4
sharpdeveloper <nifi@4simpleemail.com>
2019-07-12 04:34:10 +0530

    updated file

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress_new_01' );
+define( 'DB_NAME', 'ExampleProject' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
